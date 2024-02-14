e3bca17225ce32d2b6627e7945761fe11226a234
na <sudaphornsahnguandi5488@gmail.com>
2024-02-14 11:36:23 +0700

    add app01 app02

---
+++ streamlit/app02.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title('OOP in ML')
+st.sidebar.text_input('Sidebar')
+st.sidebar.button('go>')
+st.sidebar.button('went>')
+st.sidebar.button('gone>')
+st.image('https:/picsum.photos/500/200')
+st.text_input("What's your name")
